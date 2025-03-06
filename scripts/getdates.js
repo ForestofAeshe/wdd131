8162d8295ca936763706075371e1919de1a0718b
ForestofAeshe <msawpetersen@gmail.com>
2025-03-05 21:36:25 -0700

    work on assingment 1 week 1

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+
+
+const full = document.querySelector("#full");
+const year = document.querySelector("#year");
+let text = 
+
+const today = new Date();
+
+
+
+year.innerHTML = 'getFullYear(): <span id="currentyear">$today.getFullyear()}</span>';
