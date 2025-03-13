741645adb3f289f4d843c77f091a691329a675c9
ForestofAeshe <msawpetersen@gmail.com>
2025-03-12 18:50:19 -0600

    learning activity partial completion commit

---
+++ week02/w02-learning-javascript-constructs.js
@@ -0,0 +1,23 @@
+const DAYS = 6;
+const LIMIT = 30;
+let studentReport = [11, 42, 33, 64, 29, 37, 44];
+
+// Write a for loop that will iterate through the studentReport array and print to the console the current array value if it is below 30.
+
+
+
+// Repeat the previous programming snippet by using a while loop.
+
+
+
+// Repeat the previous programming snippet by using a forEach loop.
+
+
+
+// Repeat the previous programming snippet by using a for...in loop.
+
+
+
+// Use any type of repetition (looping) statement to dynamically produce the day names (Monday, Tuesday, Wednesday, etc.) of the next number of DAYS from today's date.
+
+
