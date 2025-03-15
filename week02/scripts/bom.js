67e8e2be3e0499dadd13ce354dac30c0829b5133
ForestofAeshe <msawpetersen@gmail.com>
2025-03-14 22:13:49 -0600

    all learn activities completed. still having issues with bom.js/html delete button

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,27 +1,29 @@
-console.log('loaded');
+// console.log('loaded');
 
 const list = document.querySelector('#list');
 const button = document.querySelector('button');
-console.log(button);
+// console.log(button);
 const input = document.querySelector('#favchap');
 
-function addListItem(event) {
+const deleteButton = document.createElement('button');
+deleteButton.textContent = '❌';
+
+button.addEventListener('click', function(){
     if (input.value.trim() !== '')
-    {
-        console.log("test")
-        const li = document.createElement('li');
+        {
+            // console.log("test")
+            const li = document.createElement('li');
 
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = '❌';
-        
-        li.textContent = input.value;
-        li.append(deleteButton);
-        list.append(li);  
-    }
-}
+            li.textContent = input.value;
+            li.append(deleteButton);
+            list.append(li);  
+        }
+});
 
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
 
-
-button.addEventListener('click', addListItem);
-
+    input.value = '';
+    input.focus();
+});
 
